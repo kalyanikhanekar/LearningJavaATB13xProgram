210b763d59cf6c144b233b19838844360f5b16fe
kalyani khanekar <khanekarkalyani30@gmail.com>
2025-06-05 16:11:33 +0530

    Lab001 and Lab002

---
+++ src/ex_01_Java_Basic/Lab002_AdditionOfNumbers.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Lab002_AdditionOfNumbers {
+    public static void main(String[] args) {
+        int a=19;
+        int b=20;
+        int c;
+        c=a+b;
+        System.out.println("Addition is  "+c);
+    }
+}
