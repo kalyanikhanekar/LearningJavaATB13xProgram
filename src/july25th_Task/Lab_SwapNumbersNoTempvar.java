377388d11ebef21dbbc5be2c246b141ffcb9208c
kalyani khanekar <khanekarkalyani30@gmail.com>
2025-07-25 10:29:44 +0530

    25th July

---
+++ src/july25th_Task/Lab_SwapNumbersNoTempvar.java
@@ -0,0 +1,16 @@
+package july25th_Task;
+
+public class Lab_SwapNumbersNoTempvar {
+    public static void main(String[] args) {
+
+
+        int a=208;
+        int b=404;
+
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a+"  "+b);
+
+    }
+}
