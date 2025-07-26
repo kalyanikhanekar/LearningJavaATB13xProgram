cc24f0e9903abfbdd129212c81aaa0bd1a9cf528
kalyani khanekar <khanekarkalyani30@gmail.com>
2025-07-26 22:18:06 +0530

    26th July

---
+++ src/july26th_Task/Lab_Factorial.java
@@ -0,0 +1,18 @@
+package july26th_Task;
+
+public class Lab_Factorial {
+    public static void main(String[] args) {
+
+
+        int n=4;
+        int fact=1;
+        for(int i=1;i<=n;i++){
+
+
+                fact=fact *i;
+
+
+        }
+        System.out.println(fact);
+    }
+}
